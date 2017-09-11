cee425926b6475c3256e6f9b3351653222e7b1e8
sairagula <abdukhalieva@gmail.com>
2017-09-11 10:08:00 -0700

    class robot

--- 02-robot-name/robot_name.rb
+++ 02-robot-name/robot_name.rb
@@ -1,1 +1,15 @@
+class Robot
+  attr_reader :name
 
+  def initialize
+    reset
+  end
+
+  def reset
+    @name = [*('A'..'Z')].sample(2).join + [*(0..9)].sample(3).join
+  end
+
+end
+
+# pattern = '[aw-zX][123]'
+# result = StringRandom.random_regex(pattern)
